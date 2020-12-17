fbbd3b27a6d0db014e67694839201ad8143dfe73
João Hugo Leda <joaohugoleda2@gmail.com>
2020-12-16 23:40:09 -0300

    aula 04

--- Exercícios/Aula04/index.js
+++ Exercícios/Aula04/index.js
@@ -1,5 +1,13 @@
+/*
 import { createServer } from 'http'
 
 createServer(function(req, res){
     res.end(`Gerenciador`)
 }).listen(8080)
+*/
+
+var http = require('http')
+
+http.createServer(function(req, res){
+    res.end('Gerenciador')
+}).listen(800)
