3cfa8a48750fe9af92eb5471ca71e3e6e57d569f
João Hugo Leda <joaohugoleda2@gmail.com>
2020-12-17 20:51:43 -0300

    Create Server

--- Exercícios/Aula04/index.js
+++ Exercícios/Aula04/index.js
@@ -1,6 +1,22 @@
 const http = require('http')
 
-const server = http.createServer()
+const server = http.createServer( (req, res) => {
+    const resp = []
+    resp['/'] = '<h1>Home</h1>'
+    resp['/contato'] = '<h1>Contato</h1>'
+    resp['semURL'] = 'Sem resposta para essa URL'
+
+    res.end(resp[req.url] || resp['semURL'])
+    /*
+    if(req.url == '/') {
+        res.end('<h1>Home</h1>')
+    } else if (req.url == '/contato') {
+        res.end('<h1>Contato</h1>')
+    } else {
+        res.end('Sem resposta para essa URL')
+    }
+    */
+})
 
 server.listen(3001, 'localhost', () => {
     console.log('Servidor de pé em: localhost:3001')
