4231844b9df4c872068e6a6f6184b5c962db3b7a
João Hugo Leda <joaohugoleda2@gmail.com>
2021-01-04 22:29:53 -0300

    Sequelize

--- aula011/app.js
+++ aula011/app.js
@@ -1,7 +1,27 @@
+const { Sequelize } = require('sequelize')
+
+const sequelize = new Sequelize('celke', 'curso', '040303', {
+    host        : 'localhost',
+    dialect     : 'mysql'
+})
+
+sequelize.authenticate().then(function () {
+    console.log('Conexão realizada com sucesso!')
+}).catch(function (error) {
+    console.log('Erro ao realizar conexão: ' + error)
+})
+
+/*
 const Sequelize = require('sequelize')
 
 const sequelize = new Sequelize('celke', 'curso', '040303', {
     host        : 'localhost',
-    dialect    : 'mysql'
+    dialect     : 'mysql'
 })
 
+sequelize.authenticate().then(function () {
+    console.log(`Conexão realizada com sucesso!`)
+}).cath(function (error) {
+    console.log('Erro ao realizar a conexão: ' + error)
+})
+*/
