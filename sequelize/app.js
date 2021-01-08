a6bb65d90a00cb1da097c0c8993157c3b1323dbf
João Hugo Leda <joaohugoleda2@gmail.com>
2021-01-08 00:22:54 -0300

    Sequelize define and create

--- sequelize/app.js
+++ sequelize/app.js
@@ -5,9 +5,35 @@ const sequelize = new Sequelize('celke', 'curso', '040303', {
   dialect   : 'mysql' 
 })
 
+module.exports = sequelize;
+
 try {
     sequelize.authenticate()
     console.log('Connection has been established successfully.')
   } catch (error) {
     console.error('Unable to connect to the database:', error)
 }
+
+const Produto = sequelize.define('produto', {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true
+  },
+  nome: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  preco: {
+    type: Sequelize.DOUBLE,
+  }  
+})
+
+//Criar tabela com sequelize
+//Produto.sync({  force:  true  })
+
+Produto.create({
+  nome: 'Condicionador',
+  preco: 19.20
+})
